119637686b04441c782d7e3b0b1ee457277043f4
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-01-10 23:34:22 -0500

    initial commit.

---
+++ autoencoder.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt 
+
+# import mnist from tensorflow datasets
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
